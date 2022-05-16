c05c0e66f3f5fb3343845f9dfbad7c5aa10092be
Ashmita <kca1@nku.edu>
2022-05-16 10:52:17 -0400

    Initial Commit

---
+++ Characters/CharacterRow.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+import Kingfisher
+
+struct CharacterRow: View {
+    
+    var character: MarvelCharacter
+    
+    var body: some View {
+        VStack {
+            HStack {
+                KFImage(character.thumbnail?.getImageURL())
+                    .resizable()
+                    .frame(width: 32, height: 32)
+                    .cornerRadius(6)
+                if(character.name == nil){
+                    Text("Unknown")
+                }
+                else{
+                    Text(character.name!)
+                }
+                
+            }
+        }
+    }
+}
+
+struct CharacterRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CharacterRow(character: MarvelCharacter.ironMan)
+    }
+}
